49acf327c28353a0fc03310892213ea69cb681e5
julia puzdrowska/Julesqo <julia.kaczor@o2.pl>
2019-03-23 11:27:23 +0100

    kom

---
+++ src/main/java/com/javagda21/wzorce/creational/singleton/zadanie1/Rejestracja.java
@@ -0,0 +1,4 @@
+package com.javagda21.wzorce.creational.singleton.zadanie1;
+
+public class Rejestracja {
+}
---
+++ src/main/java/com/javagda21/wzorce/creational/singleton/zadanie1/zadanie2/Main.java
@@ -0,0 +1,4 @@
+package com.javagda21.wzorce.creational.singleton.zadanie1.zadanie2;
+
+public class Main {
+}

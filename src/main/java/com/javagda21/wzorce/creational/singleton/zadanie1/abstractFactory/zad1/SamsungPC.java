676049ad813f067c4b42a2a0fba1c6c610a3d0c9
julia puzdrowska/Julesqo <julia.kaczor@o2.pl>
2019-03-23 12:33:48 +0100

    Factory

---
+++ src/main/java/com/javagda21/wzorce/creational/singleton/zadanie1/abstractFactory/zad1/SamsungPC.java
@@ -0,0 +1,4 @@
+package com.javagda21.wzorce.creational.singleton.zadanie1.abstractFactory.zad1;
+
+public class SamsungPC {
+}
---
+++ src/main/java/com/javagda21/wzorce/creational/singleton/zadanie1/creational/abstractFactory/zadanie2/BikeType.java
@@ -0,0 +1,4 @@
+package com.javagda21.wzorce.creational.singleton.zadanie1.creational.abstractFactory.zadanie2;
+
+public class BikeType {
+}
